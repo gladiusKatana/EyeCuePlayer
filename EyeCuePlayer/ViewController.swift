94512c7b90b52680d8c13f321e6ddfa26d823535
gladiusKatana <garthsnyder@protonmail.com>
2020-02-10 21:16:33 -0500

    Initial Commit

---
+++ EyeCuePlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EyeCuePlayer
+//
+//  Created by Garth Snyder on 2020-02-10.
+//  Copyright © 2020 Garth Snyder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
